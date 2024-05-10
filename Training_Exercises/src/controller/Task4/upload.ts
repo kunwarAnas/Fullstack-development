e4b9245e833e71aa400896a9269b09f9ca2fd724
KunwarAnasAli <kunwar.ali@tothenew.com>
2024-05-10 16:55:32 +0530

    task 4 upload download controller added

--- Training_Exercises/src/controller/Task4/upload.ts
+++ Training_Exercises/src/controller/Task4/upload.ts
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import { S3 } from 'aws-sdk';
 import { Request, Response } from 'express';
 
@@ -6,7 +7,7 @@ export const uploadToS3 = async (req: Request, res: Response) => {
 
     try {
         const fileData = req.body.fileData;
-        const fileName = req.body.fileName; 
+        const fileName = req.body.fileName;
 
         if (!fileData || !fileName) {
             return res.status(400).json({ error: 'Missing file data or file name' });
@@ -18,24 +19,51 @@ export const uploadToS3 = async (req: Request, res: Response) => {
             Body: `${JSON.stringify(fileData)}.json`,
         };
 
+        console.log(process.env.env)
 
-        const s3 = new S3({
-            accessKeyId: process.env.accessKeyId,
-            secretAccessKey: process.env.secretAccessKey,
-        });
+        if (process.env.env !== 'local') {
+            const s3 = new S3({
+                accessKeyId: process.env.accessKeyId,
+                secretAccessKey: process.env.secretAccessKey,
+            });
 
-        return s3.upload(s3Params, (err: any, data: {}) => {
-            if (err) {
-                console.log(err.message)
-                throw new Error(err.message)
-            }
+            return s3.upload(s3Params, (err: any, data: {}) => {
+                if (err) {
+                    console.log(err.message)
+                    throw new Error(err.message)
+                }
 
-            return res.status(200).send('S3 upload success')
-        });
+                return res.status(200).send('S3 upload success')
+            });
+
+        } else {
+            fs.writeFile(__dirname + '/demo.json', JSON.stringify(fileData), (err) => {
+                if (!err) {
+                    res.status(200).json({
+                        message: 'file create success',
+                        data: fileData
+                    })
+                }
+            })
+        }
 
     } catch (error: any) {
         console.error('Error accessing S3 or processing CSV:', error);
         res.status(400).send(error.message);
     }
 
+}
+
+export const download = async (req: Request, res: Response) => {
+    try {
+        // Set headers to trigger file download
+        res.setHeader('Content-Disposition', `attachment; filename="demoFile.json"`);
+        res.setHeader('Content-Type', 'application/octet-stream'); // Set the appropriate content 
+        const fileStream = fs.createReadStream(__dirname + '/demo.json');
+        fileStream.pipe(res);
+    } catch (err) {
+        if (err instanceof Error) res.status(500).json({
+            message: err?.message && err.message
+        })
+    }
 }
